24aa5d31b78cb8dd69e9823b566d3021093a6b47
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2020-08-24 07:23:14 +0300

    still working on ll-insertion

--- data_structures_and_algorithm/linked_list/linked_list.py
+++ data_structures_and_algorithm/linked_list/linked_list.py
@@ -77,7 +77,62 @@ class LinkedList():
             print (output)
         except Exception as error:
             print (f"There is error in __init__ of LinkedList, the error {error}")
+    
+    def append(self,value):
+        """
+        This is append method of LinkedList,
+        to add all values for LinkeList.
+        """
+        try:
+            new_node=Node(value)
+            if self.head==None:
+                self.head=new_node
+            else:
+                current=self.head
+                while current.next != None:
+                    current=current.next
+                current.next=new_node
 
+        except Exception as error:
+            print (f"There is an error in append of LinkList {error}")
+        
+    def insert_before(self,next_node,new_val):
+        """
+        This is append method of LinkedList,
+        to add all values before the first node.
+        """
+        try:
+            if next_node == None:
+                print("Doesn't exist")
+            new_node=Node(value)
+            current=self.head
+            if next_node==current:
+                new_node=Node(value)
+                new_node.next=self.head
+                self.head=new_node
+            while current:
+                if current.next==next_node:
+                    next_node.next=current.next
+                    current.next=new_node
+                else:
+                    current=current.next
+        except Exception as error:
+            print (f"There is an error in append of LinkList {error}")
+        
+    def insert_after(self,pre_node,new_val):
+        """
+        This is append method of LinkedList,
+        to add all values after the first node.
+        """
+        try:
+            if pre_node==None:
+                print("doesn't exist")
+            new_node=Node(value)
+            new_node.next=pre_node.next
+            pre_node.next=new_node
+
+        except Exception as error:
+            print (f"There is an error in append of LinkList {error}")
 
 if __name__=="__main__":
     list1= LinkedList()
@@ -90,3 +145,7 @@ if __name__=="__main__":
     print(list1.includes("99",99),"222222")
     list1.toString()
     print(list1)
+    list1.insert_before(list1.head.next,'15')
+    print(list1.toString())
+    list1.insert_after(list1.head.next.next,6)
+    print(list1.toString())

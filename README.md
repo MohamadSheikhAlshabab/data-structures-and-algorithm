647f57562d38e89f85e57f3e25060a008f78ccc2
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2020-08-29 23:12:04 +0300

    Done ll-insertion

---
+++ README.md
@@ -0,0 +1,14 @@
+# ll-insertions
+
+insert on Linked List
+
+## Challenge
+
+Big o(1) for insert, the worst case scenario is O(n)
+
+## Approach & Efficiency
+<!--  -->
+
+## Solution
+
+![img]("https://drive.google.com/file/d/1zuGJKppUphyNPpqeK_pdYET2ACI2tY70/view?usp=sharing")
---
+++ tests/test_linked_list.py
@@ -0,0 +1,56 @@
+from data_structures_and_algorithm.Data_Structures.linked_list import LinkedList,Node
+
+
+def test_empty_list():
+    ll = LinkedList()
+    assert ll.head == None
+
+def test_insert_to_empty_ll():
+    ll = LinkedList()
+    ll.insert(5)
+    assert ll.head.value == 5
+
+def test_multiple_insert():
+    ll = LinkedList()
+    ll.insert(5)
+    ll.insert(6)
+    ll.insert(7)
+    assert ll.to_string() == 'head-> {5}->{6}->{7}->NULL'
+
+def test_includes():
+    ll = LinkedList()
+    ll.insert(5)
+    ll.append_value(55)
+    assert ll.includes(5) == True
+
+def test_includes_false():
+    ll = LinkedList()
+    ll.insert(5)
+    ll.insert(77)
+    ll.insert(75)
+    assert ll.includes(6) == False
+
+def test_dender_str():
+    ll = LinkedList()
+    ll.insert(5)
+    ll.insert(6)
+    ll.insert(7)
+    assert ll.to_string() == 'head-> {5}->{6}->{7}->NULL'
+
+def test_insert_before():
+    ll = LinkedList()
+    ll.insert(5)
+    ll.insert(6)
+    ll.insert(8)
+    ll.insert(9)
+    ll.insert_before(6,7)
+    assert ll.to_string() == 'head-> {5}->{7}->{6}->{8}->{9}->NULL'
+
+def test_insert_after():
+    ll = LinkedList()
+    ll.insert(5)
+    ll.insert(6)
+    ll.insert(8)
+    ll.insert(9)
+    ll.insert_after(8,7)
+    assert ll.to_string() == 'head-> {5}->{6}->{8}->{7}->{9}->NULL'

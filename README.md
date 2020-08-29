0a28e00fa508b3409982671181faf4c987eedddc
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2020-08-29 21:32:23 +0300

    DONE Linked_List

---
+++ README.md
@@ -0,0 +1,14 @@
+# Linked List
+
+create Linked List has a lot of methods like insert, includes, to_string.
+
+## Challenge
+
+Big o(1) for insert, Big o(n) for include
+
+## Approach & Efficiency
+<!--  -->
+
+## Solution
+
+![img]("https://drive.google.com/file/d/1IbAwkfj7YXXMS4EzS6BaYuEAxhwxfgti/view?usp=sharing")
---
+++ tests/test_linked_list.py
@@ -0,0 +1,44 @@
+from data_structures_and_algorithms.Data_Structures.linked_list import LinkedList,Node
+import pytest
+
+@pytest.fixture
+def data():
+    linkedlist=LinkedList()
+    return {'linked':linkedlist}
+
+def test_empty_list():
+    expected=None
+    actual = LinkedList().items()
+    assert actual==expected
+
+def test_insert(data):
+    expected=3
+    data["linked"].insert(5)
+    data["linked"].insert(55)
+    data["linked"].insert(555)
+    actual=data["linked"].items()
+    assert actual==expected
+
+def test_multi_insert(data):
+    data["linked"].insert(5)
+    data["linked"].insert(50)
+    data["linked"].insert(13)
+    data["linked"].insert(222)
+    data["linked"].insert(99)
+    expected=" {5}->{50}->{13}->{222}->{99}->NULL"
+    actual = data["linked"].to_string()
+    assert actual==expected
+    
+def test_insert_string(data):
+    data["linked"].insert(5)
+    expected=" {5}->NULL"
+    actual=data["linked"].to_string()
+    assert actual==expected
+
+def test_includes(data):
+    expected=True
+    data["linked"].insert(5)
+    data["linked"].insert(5)
+    actual=data["linked"].includes(5)
+    assert actual==expected
+

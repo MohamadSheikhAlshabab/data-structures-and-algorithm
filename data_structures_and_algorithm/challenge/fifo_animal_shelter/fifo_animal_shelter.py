d22c2566283bb64a1a54b478ddd4633ed0969818
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2020-09-04 12:42:58 +0300

    done fifo-animal-shelter

--- data_structures_and_algorithm/challenge/fifo_animal_shelter/fifo_animal_shelter.py
+++ data_structures_and_algorithm/challenge/fifo_animal_shelter/fifo_animal_shelter.py
@@ -1,76 +1,133 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
 class Animal_Shelter:
     def __init__(self):
-        self.front = None
-        self.rear = None
-        self.next = None
-
-    def __str__(self):
         """
-        This is toString method of LinkedList,
-        to return all values as string.
+        This is __init__ method of Animal_Shelter.
+        it has two attribute, self.cats, and self.dogs,
+        they're assigen to the class Queue.
         """
-        current = self.front
-        output = ''
-        while current:
-            output += f"{current.value}->"
-            current = current.next
-        return output
+        self.cats=Queue()
+        self.dogs=Queue()
 
-    def enqueue(self, value):
+    def enqueue(self,animal_name,animal_type):
         """
         This is enqueue method of class Animal_Shelter,
-        it has two attribute, they're called new_node, temp.
+        it has two attribute, they're called animal_name, animal_type.
         push method use to add an item to the Queue.
         :return: None
         """
-        
-        new_node = Node(value)
-        if not self.front and not self.rear:
-            self.front = new_node
-            self.rear = new_node
+        try:
+            if animal_type.upper() == 'CAT':
+                new_animal=Cat(animal_name)
+                self.cats.enqueue(new_animal)
+            elif animal_type.upper()  == 'DOG':
+                new_animal=Dog(animal_name)
+                self.dogs.enqueue(new_animal)
+            else :
+                return None
+        except Exception as err:
+            print(f'There is error in enqueue method of class Animal_Shelter : {err}')
 
-        old_rear = self.rear
-        self.rear = new_node
-        old_rear.next = self.rear
 
-    def dequeue(self,pref):
+    def dequeue(self,animal_type):
         """
-        This is dequeue method of class Animal_Shelter,
-        it has one attribute, it's called value.
-        dequeue method use to pop an item from the front of Queue.
-        :return: None
+        removes the node from the front of the queue, and returns the node’s value
         """
-        pass
+        try:
+            if animal_type.upper() == 'CAT':
+                return self.cats.dequeue()
+            elif animal_type.upper() == 'DOG':
+                return self.dogs.dequeue()
+            else :
+                return None
+        except Exception as err:
+            print(f'There is error in dequeue method of class Animal_Shelter : {err}')
 
 class Cat:
-    def __init__(self):
-        pass
+    def __init__(self,value):
+        """
+        This is __init__ method of the class Cat.
+        it has one attribute, it's called value.
+        """
+        self.value=value
+        self.type="cat"
+        self.next=None
 
 class Dog:
+    def __init__(self,value):
+        """
+        This is __init__ method of the class Dog.
+        it has one attribute, it's called value.
+        """
+        self.value=value
+        self.type="dog"
+        self.next=None
+
+
+class Queue:
+    """
+    this class will create a queue
+    """
     def __init__(self):
-        pass
+        self.front = None
+        self.rear = None
+
+    def enqueue(self,new_animal):
+        """
+        This is enqueue method of class Queue,
+        it has one attribute, it's called new_animal.
+        push method use to add an item to the Queue.
+        :return: None
+        """
+        try:
+            new_node = new_animal
+            if not self.front and not self.rear:
+                self.front = new_node
+                self.rear = new_node
+            old_rear = self.rear
+            self.rear = new_node
+            old_rear.next = self.rear
+        except Exception as err:
+            print(f'There is error in enqueue method of class Queue : {err}')
+        
+
+    def dequeue(self):
+        """
+        removes the node from the front of the queue, and returns the node’s value
+        """
+        try:
+            first_node = self.front
+            self.front = self.front.next
+            return first_node.value
+        except Exception as err:
+            print(f'There is error in dequeue method of class Queue : {err}')
+            return "Queue is empty"
+
+    def __str__(self):
+        """
+        this method creates a readable output for the user
+        """
+        try:
+            output = ''
+            current = self.front
+            while current: 
+                output += f"<{current.value}->"
+                current = current.next
+            output += f"{current}"
+            return output
+        except Exception as err:
+            print(f'This is error in __str__: {err}')
+
 
 if __name__ == "__main__":
-    matrix = Dog()
-    poppy = Dog()
-    sherry = Cat()
-    bob = Dog()
-    blacky = Cat()
-    henry = Dog()
     shelter = Animal_Shelter()
-    shelter.enqueue(matrix)
-    shelter.enqueue(poppy)
-    shelter.enqueue(sherry)
-    shelter.enqueue(bob)
-    shelter.dequeue('Dog')
-    shelter.enqueue(blacky)
-    # print(shelter.dequeue('cat'))
-    shelter.enqueue(henry)
-    # print(shelter.dequeue('Dog'))
-    # print(shelter.dequeue('cat'))
-    print(shelter.__str__())
+    shelter.enqueue("matrix","dog")
+    shelter.enqueue("poppy","dog")
+    shelter.enqueue("sherry","dog")
+    shelter.enqueue("bob","dog")
+    shelter.enqueue("blacky","cat")
+
+    print(shelter.dequeue('dog'))
+    print(shelter.dequeue('dog'))
+    print(shelter.dequeue('cat'))
+    print(shelter.dequeue('catff'))
+

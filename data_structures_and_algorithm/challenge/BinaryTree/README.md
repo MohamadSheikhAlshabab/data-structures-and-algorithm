ce2c09164a1e1dd44a4cae43401bfb87fce37d6f
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2020-09-05 14:11:10 +0300

    done

---
+++ data_structures_and_algorithm/challenge/BinaryTree/README.md
@@ -0,0 +1,24 @@
+# find-maximum-binary-tree
+
+we  want to find the maximum value exists in the binary tree.
+
+## Challenge
+
+<code><pre>           2
+                    /   \
+                   7     5
+                  / \      \
+                 2   6      9
+                    / \     /
+                   5   11  4
+
+</pre></code>
+
+output ==>> 11
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## API
+
+![img]("https://drive.google.com/file/d/1n3ZIiqDaibk-tuXpBSe0r7u6L7MkD27F/view?usp=sharing")
---
+++ data_structures_and_algorithm/challenge/BinaryTree/binary_tree.py
@@ -0,0 +1,134 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.right=None
+        self.left=None
+
+class Binary_Tree:
+    def __init__(self,root):
+        self.root=Node(root)
+
+    def pre_order(self,start,traversal):
+        """
+        this is the first mode of depth of tree,fill the tree starting from :
+         ROOT -->> LEFT -->> RIGHT.
+        """
+        try:
+            output=[]
+            
+            if start:
+                traversal += str(start.value)
+                traversal = self.pre_order(start.left,traversal)
+                traversal = self.pre_order(start.right,traversal)
+            
+            x= output.append(traversal)
+            c= output[0]
+            return c
+        except Exception as error:
+            print(f"Thsre is error in pre_order:{error}")
+
+    def in_order(self,start,traversal):
+        """
+        this is the second mode of depth of tree,fill the tree starting from :
+         LEFT-->> ROOT -->> RIGHT.
+        """
+        try:
+            if start:
+                traversal = self.in_order(start.left,traversal)
+                traversal += (f" {str(start.value)} -->>")
+                traversal = self.in_order(start.right,traversal)
+            # print ("in_order : ")
+            return traversal
+        except Exception as error:
+            print(f"Thsre is error in in_order:{error}")
+
+    def post_order(self,start,traversal):
+        """
+        this is the third mode of depth of tree,fill the tree starting from :
+          LEFT -->> RIGHT -->> ROOT.
+        """
+        try:
+            if start:
+                traversal = self.post_order(start.left,traversal)
+                traversal = self.post_order(start.right,traversal)
+                traversal += (f" {str(start.value)} -->>")
+            # print ("post_order : ")
+            return traversal
+        except Exception as error:
+            print(f"Thsre is error in post_order:{error}")
+
+    def add(self,value):
+        try:
+            if not self.root:
+                self.root=Node(value)
+            else:
+                new_node=Node(value)
+                if new_node>=self.root:
+                    self.root.right=new_node
+                    return self.root.right
+                elif new_node<self.root:
+                    self.root.left=new_node
+                    return self.root.left
+        except Exception as error:
+            print(f"Thsre is error in add:{error}")
+
+    def contains (self,type_depth):
+        try:
+            if type_depth == "pre_order":
+                return self.pre_order(self.root,"")
+            elif type_depth == "in_order":
+                return self.in_order(self.root,"")
+            elif type_depth == "post_order":
+                return self.post_order(self.root,"")
+            else:
+                print("Wrong type depth")
+                return False 
+        except Exception as error:
+            print(f"There is error in contains :{error}")
+
+    def find_maximum_value(self,root_value):
+        """
+        Finds the maximum value in the binary tree.
+        return: the maximum value.
+        
+        """
+        try:
+            if not self.root:
+                return 'Tree is empty'
+        
+            else:
+                max_value = root_value.value
+
+                if root_value.left:
+                    left_max = self.find_maximum_value(root_value.left)
+                    if left_max > max_value:
+                        max_value = left_max
+
+                if root_value.right:
+                    right_max = self.find_maximum_value(root_value.right)
+                    if right_max > max_value:
+                        max_value = right_max
+            
+            return max_value
+        except Exception as error:
+            print(f"There is error in find amximum value :{error}")
+
+
+if __name__ == "__main__":
+    tree = Binary_Tree(2)
+    tree.root.left = Node(7)
+    tree.root.left.left = Node(2)
+    tree.root.left.right = Node(6)
+    tree.root.left.right.left = Node(5)
+    tree.root.left.right.right = Node(11)
+    tree.root.right = Node(5)
+    tree.root.right.right= Node(9)
+    tree.root.right.right.left = Node(4)
+    # tree.add(15)
+    # print(tree.add(15))
+    print("pre_order",tree.contains("pre_order"))
+    print("in_order",tree.contains("in_order"))
+    print("post_order",tree.contains("post_order"))
+
+    print(tree.find_maximum_value(tree.root))
+ 
